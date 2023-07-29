b53373be356762714d7287c16621b001e712f7e6
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-29 10:48:00 +0300

    sockets-server

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
 
         //the purpose of the origin is to tell cors that it is ok to accept communication from the react localhost 3000
 
-        origin:'https://ryanchats.vercel.app/',
+        origin:'http://localhost:3000/',
         methods:['GET', 'POST']
 
     }
